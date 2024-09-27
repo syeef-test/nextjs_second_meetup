38f71db5eada09b363a84f37abf39a51bfec7251
syeef <syeefislam@yahoo.com>
2024-09-27 21:06:22 +0530

    get_static_paths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -12,4 +12,46 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        id: meetupId,
+        alt: "a first meetup",
+        title: "A first meetup",
+        address: "Some street 5,Some city",
+        description: "he meetup description",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,16 @@ function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
 export async function getStaticProps() {
   return {
     props: {
