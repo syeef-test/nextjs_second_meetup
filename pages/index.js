a95f1c47371099ed11a25406757f851ff06b94ee
syeef <syeefislam@yahoo.com>
2024-09-27 20:20:38 +0530

    getstaticprops

--- pages/index.js
+++ pages/index.js
@@ -27,8 +27,17 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 10,
+  };
 }
 
 export default HomePage;
