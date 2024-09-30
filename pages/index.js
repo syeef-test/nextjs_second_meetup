4ac292f541f5a3ff2f9b3dbcdd62f8167be3d525
syeef <syeefislam@yahoo.com>
2024-09-30 20:35:48 +0530

    meetups data fetched from mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,32 @@
 import MeetupList from "@/components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A first meetup",
-    image:
-      "https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    address: "Some address,12345",
-    description: "This is a first meetup!",
-  },
-  {
-    id: "m2",
-    title: "A second meetup",
-    image:
-      "https://images.pexels.com/photos/3989821/pexels-photo-3989821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    address: "Some address,12322145",
-    description: "This is a second meetup!",
-  },
-  {
-    id: "m3",
-    title: "A third meetup",
-    image:
-      "https://images.pexels.com/photos/257499/pexels-photo-257499.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    address: "Some address,12wwdw345",
-    description: "This is a third meetup!",
-  },
-];
+// const DUMMY_MEETUPS = [
+//   {
+//     id: "m1",
+//     title: "A first meetup",
+//     image:
+//       "https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     address: "Some address,12345",
+//     description: "This is a first meetup!",
+//   },
+//   {
+//     id: "m2",
+//     title: "A second meetup",
+//     image:
+//       "https://images.pexels.com/photos/3989821/pexels-photo-3989821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     address: "Some address,12322145",
+//     description: "This is a second meetup!",
+//   },
+//   {
+//     id: "m3",
+//     title: "A third meetup",
+//     image:
+//       "https://images.pexels.com/photos/257499/pexels-photo-257499.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//     address: "Some address,12wwdw345",
+//     description: "This is a third meetup!",
+//   },
+// ];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
@@ -42,9 +43,27 @@ function HomePage(props) {
 // }
 
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://kazi:nkdhpwuqDfSvOIQc@cluster0.qeakjte.mongodb.net/next_js_meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("next_js_meetups");
+  const meetups = await meetupsCollection.find().toArray();
+
+  console.log(meetups);
+
+  await client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 10,
   };
