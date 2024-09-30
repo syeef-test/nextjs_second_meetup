bcc238625c07761164e5bf3baad5593666c6eb0a
syeef <syeefislam@yahoo.com>
2024-09-30 20:08:14 +0530

    data added to mongodb succesfully

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,26 @@
+import { MongoClient } from "mongodb";
 // /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    // const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://kazi:nkdhpwuqDfSvOIQc@cluster0.qeakjte.mongodb.net/next_js_meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("next_js_meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    await client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
